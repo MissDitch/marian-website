47b22f4e22347a1cb0840370a153faa0a494d017
Marian Sloot <mjtsloot@icloud.com>
2018-11-03 12:53:28 +0100

    all files in directory

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).foundation()
+$(function() {
+$(window).scroll(function() {
+    var winTop = $(window).scrollTop();
+    if (winTop >= 30) {
+    $("body").addClass("sticky-shrinknav-wrapper");
+    } else{
+    $("body").removeClass("sticky-shrinknav-wrapper");
+    }
+});
+$('[data-curtain-menu-button]').click(function(){
+    $('body').toggleClass('curtain-menu-open');
+    })
+
+
+});
+
+
+
+
+
+
+
+
